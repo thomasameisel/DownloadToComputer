90ea984d9e0dc236d7a0f106426a4cba562f1361
Live session user <thomas.meisel95@gmail.com>
2015-07-20 23:38:43 +0000

    bug fixes

--- download_links.py
+++ download_links.py
@@ -18,7 +18,7 @@ class Downloader(object):
     def __init__(self, auth_key, device_name, last_push = time.time(), device_iden=None):
         self._auth_key = auth_key
         self.pb = PushBullet(self._auth_key)
-        self.listener = Listener(self.pb, self.check_pushes)
+        self.listener = Listener(self.pb, self.download_link)
 
         self.last_push = last_push
 
@@ -36,37 +36,45 @@ class Downloader(object):
                 print("Error: Unable to create device")
                 raise
 
-    def check_pushes(self, push):
-        pushes = self.pb.get_pushes(self.last_push)
+    def get_unique_filename(self, filename, downloads_dir):
+        unique_filename = filename
+        i = 0
+        while os.path.isfile(downloads_dir+"/"+unique_filename):
+            i+=1
+            unique_filename=filename+" (%i)" % i
+        return unique_filename
+
+    def get_filename_from_url(self, url):
+        if url.rfind('/') != -1:
+            title = url[url.rfind('/')+1:]
+        if len(title) < 1:
+            try:
+                response = requests.get(url)
+                parsed_body = html.fromstring(response.text)
+                titles = parsed_body.xpath('//title/text()')
+                if len(titleArray) == 0:
+                    title="tmptitle"
+                else:
+                    title=titles[0]
+            except:
+                title="tmptitle"
+        return title
+
+    def download_link(self, push):
+        pushes = self.pb.get_pushes(self.last_push)[1]
         if (len(pushes) != 0):
-            push = pushes[len(pushes)-2]
-            if push.get("url"):
-                url = push.get("url")
-                if url.rfind('/') != -1:
-                    title = url[url.rfind('/')+1:]
-                if len(title) < 1:
-                    try:
-                        response = requests.get(url)
-                        parsed_body = html.fromstring(response.text)
-                        titleArray = parsed_body.xpath('//title/text()')
-                        if len(titleArray) == 0:
-                            title="tmptitle"
-                        else:
-                            title=titleArray[0]
-                    except:
-                        title="tmptitle"
+            last_push = pushes[len(pushes)-2]
+            if last_push.get("url"):
+                url = last_push.get("url")
+                filename = self.get_filename_from_url(url)
 
                 downloads_dir = glib.get_user_special_dir(glib.USER_DIRECTORY_DOWNLOAD)
-                uniqueTitle = title
-                i = 0
-                while os.path.isfile(downloads_dir+"/"+uniqueTitle):
-                    i+=1
-                    uniqueTitle=title+" (%i)" % i
+                unique_filename = self.get_unique_filename(filename, downloads_dir)
+                unique_filename_full_path = downloads_dir+"/"+unique_filename
 
-                download_file_location = downloads_dir+"/"+uniqueTitle
-                urllib.urlretrieve (push.get("url"), download_file_location)
-                print "URL: %s File name: %s" % (url, uniqueTitle)
-        self.last_push = max(self.last_push, push.get("created"))
+                urllib.urlretrieve (url, unique_filename_full_path)
+                print "URL: %s Downloaded: %s" % (url, unique_filename_full_path)
+            self.last_push = max(self.last_push, last_push.get("created"))
 
     def run(self):
         try:
